62d6090aa8d837aa99d06879ea5a5f1ac87b2ab3
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-12-12 12:18:53 +0000

    Added packages
    Major Updates:
    + Added sub packages for the different users
    + Created the GUI for viewing doctors ratings

---
+++ src/PatientManagementSystem/administratorsystem/viewdoctorsratings.java
@@ -0,0 +1,9 @@
+package PatientManagementSystem.administratorsystem;
+
+import javax.swing.*;
+
+public class viewdoctorsratings {
+    private JPanel panel1;
+    private JTextField viewratingtxt;
+    private JTextField viewfeedbacknotestxt;
+}
